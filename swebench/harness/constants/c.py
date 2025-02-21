0660662aada03072c94ea37990ce77504c438e3a
gireeshm <gireeshm@vayavyalabs.com>
2025-02-21 18:11:38 +0530

    fix(specs): separate different version of jq
    separate versions of jq based on build methods

--- swebench/harness/constants/c.py
+++ swebench/harness/constants/c.py
@@ -1,17 +1,38 @@
-SPECS_JQ = {
-    k: {
-        "test_cmd": "make -j$(nproc)  && make -j $(nproc) check -s",
+
+
+SPECS_JQ1 = {
+    version: {
+        "test_cmd": "autoreconf -i \
+            && ./configure --with-oniguruma=builtin && make -j$(nproc)  && make -j $(nproc) check -s",
+        "apt-pkgs": ["bison", "flex", "libtool","libonig-dev"],
     }
-    for k in ["jq-1.7rc2", "jq-1.7rc1", "jq-1.6rc1", "jq-1.5rc2", "jq-1.5rc1", "jq-1.3"]
+    for version in ["jq-1.7rc2", "jq-1.7rc1", "jq-1.6rc1"]
 }
 
-INSTALL_JQ = "git submodule update --init \
-        && autoreconf -i \
-        && ./configure --with-oniguruma=builtin"
+SPECS_JQ2 = {
+    version: {
+        "test_cmd": "autoreconf -i \
+            && ./configure --with-oniguruma=builtin && make -j$(nproc)  && make -j $(nproc) check -s",
+        "apt-pkgs": ["bison", "flex", "libtool","libonig-dev"],
+    }
+    for version in ["jq-1.6rc1"]
+}
+
+
+SPECS_JQ3 = {
+    version: {
+        "test_cmd": "autoreconf -i \
+            && ./configure && make -j$(nproc)  && make -j $(nproc) check -s",
+        "apt-pkgs": ["bison", "flex", "libtool","libonig-dev"],
+    }
+    for version in [ "jq-1.5rc1", "jq-1.5rc2", "jq-1.3"]
+}
+
+INSTALL_JQ = "git submodule update --init "
 
 
 MAP_REPO_VERSION_TO_SPECS_C = {
-    "jqlang/jq": SPECS_JQ,
+    "jqlang/jq": {**SPECS_JQ1, **SPECS_JQ2, **SPECS_JQ3},
 }
 
 MAP_REPO_TO_INSTALL_C = {
