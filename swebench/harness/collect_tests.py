846c0b5779962036270bf031c8b22d4f7b6c085b
gireeshm <gireeshm@vayavyalabs.com>
2025-02-28 16:03:58 +0530

    fix(harness): test collection script update

--- swebench/harness/collect_tests.py
+++ swebench/harness/collect_tests.py
@@ -20,57 +20,71 @@ def load_dataset(path: Path) -> List[Dict[str, Any]]:
     return dataset
 
 
-def get_tests_with_status(logs_dir: Path, instance: Dict[str, Any]):
+def get_tests_with_status(pass_logs_dir: Path, fail_logs_dir, instance: Dict[str, Any]):
     """read test_output.txt and extract tests, append these tests to instance
 
     Args:
         logs_dir: logs directory path
         instance: test instance info from dataset
     """
-    test_log_path: Path = logs_dir / instance["instance_id"] / "test_output.txt"
+    test_pass_log_path: Path = pass_logs_dir / instance["instance_id"] / "test_output.txt"
+    test_fail_log_path: Path = fail_logs_dir / instance["instance_id"] / "test_output.txt"
     try:
-        logs = test_log_path.read_text()
+        pass_logs = test_pass_log_path.read_text()
     except FileNotFoundError as e:
-        logging.error(f"Failed to read from file: {test_log_path}: {e}, skipping")
+        logging.error(f"Failed to read pass logs from file: {test_pass_log_path} {e}, skipping")
+        return
+
+    try:
+        fail_logs = test_fail_log_path.read_text()
+    except FileNotFoundError as e:
+        logging.error(f"Failed to read fail logs from file: {test_fail_log_path} {e}, skipping")
         return
 
     repo = instance["repo"]
     parser = MAP_REPO_TO_PARSER[repo]
-    tests = parser(logs, None)
-    pass_to_pass: Set[str] = set(instance.get("PASS_TO_PASS", []))
-    fail_to_pass: Set[str] = set(instance.get("FAIL_TO_PASS", []))
-    for test, status in tests.items():
-        if status == TestStatus.PASSED.value and test not in fail_to_pass:
-            pass_to_pass.add(test)
-        else:
-            fail_to_pass.add(test)
+    pass_tests = parser(pass_logs, None)
+    fail_tests = parser(fail_logs, None)
+    pass_to_pass: Set[str] = {test_name for test_name, status in pass_tests.items() if status == TestStatus.PASSED.value if fail_tests[test_name] == TestStatus.PASSED.value }
+    fail_to_pass: Set[str] = {test_name for test_name, status in fail_tests.items() if status == TestStatus.FAILED.value if pass_tests[test_name] == TestStatus.PASSED.value}
     instance["PASS_TO_PASS"] = list(pass_to_pass)
     instance["FAIL_TO_PASS"] = list(fail_to_pass)
 
 
 def main(
-    model: str, run_id: str, dataset_path: Path, output: Path, logs_base_path: Path
+    model: str,
+    pass_run_id: str,
+    fail_run_id: str,
+    dataset_path: Path,
+    output: Path,
+    logs_base_path: Path,
 ):
     logging.basicConfig(
         level=logging.ERROR, format="%(asctime)s - %(levelname)s - %(message)s"
     )
 
     dataset = load_dataset(dataset_path)
-    logs_dir = logs_base_path / run_id / model
+    pass_logs_dir = logs_base_path / pass_run_id / model
+    fail_logs_dir = logs_base_path / fail_run_id / model
 
     for instance in dataset:
-        get_tests_with_status(logs_dir, instance)
+        get_tests_with_status(pass_logs_dir, fail_logs_dir, instance)
 
-    with open(output, 'w') as f:
+    with open(output, "w") as f:
         json.dump(dataset, f)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="collect all the tests from already ran test outputs and adds to dataset"
+        description="""collect all the tests from already ran test outputs and adds to dataset
+
+        To get the results first run the test with gold predictions, then run the same tests with dummy predictions(which doesn't solves the issue).
+        Then run this script to get PASS_TO_PASS and FAIL_TO_PASS tests
+        """
     )
     parser.add_argument("model", help="model name of tests")
-    parser.add_argument("run_id", help="run id used by previous harness test")
+    parser.add_argument("pass_run_id", help="run id used by previous harness test with gold predictions")
+    parser.add_argument("fail_run_id", help="run id used by previous harness test with dummy predictions")
     parser.add_argument("dataset", help="path to dataset")
     parser.add_argument("output", help="path to output dataset")
     parser.add_argument(
@@ -83,7 +97,8 @@ if __name__ == "__main__":
 
     main(
         args.model,
-        args.run_id,
+        args.pass_run_id,
+        args.fail_run_id,
         Path(args.dataset),
         Path(args.output),
         Path(args.eval_path),
